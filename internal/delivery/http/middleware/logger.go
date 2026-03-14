912a604cdd60c80699339c7c9c938f49927f9db7
M. Fadhil Mahendra <mfmahendr@gmail.com>
2026-03-14 16:52:48 +0700

    feat: :sparkles: add error handler, API versioning, and logger middleware

---
+++ internal/delivery/http/middleware/logger.go
@@ -0,0 +1,14 @@
+package middleware
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func Logger() fiber.Handler {
+	return logger.New(logger.Config{
+		Format:     "[${time}] ${status} - ${method} ${path}\n",
+		TimeFormat: "2006-01-02 15:04:05",
+		TimeZone:   "Local",
+	})
+}
